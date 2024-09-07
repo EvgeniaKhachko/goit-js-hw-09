b1f5ae9cca59f305a5f98804f5939942691cae89
EvgeniaKhachko <167171409+EvgeniaKhachko@users.noreply.github.com>
2024-09-07 19:09:51 +0000

    Deploying to gh-pages from @ EvgeniaKhachko/goit-js-hw-09@f1b198aa700a1c1a69afe16de3c174981ba102b9 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/styles-cv9GSsrn.js";import{S as o}from"./assets/vendor-CgTBfC_f.js";let t=new o(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});t.next();console.log(o);
+import"./assets/styles-cv9GSsrn.js";import{S as t}from"./assets/vendor-CgTBfC_f.js";new t(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
