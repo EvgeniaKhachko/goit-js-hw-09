f1b198aa700a1c1a69afe16de3c174981ba102b9
kapitu <ketkap007@gmail.com>
2024-09-07 22:09:33 +0300

    hw-9-fix

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -3,31 +3,9 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-let gallery = new SimpleLightbox('.gallery a', {
+const gallery = new SimpleLightbox('.gallery a', {
     captions: true,
     captionsData: 'alt',
     captionPosition: 'bottom', 
     captionDelay: 250 
   });
-
-
-gallery.next();
-
-console.log(SimpleLightbox);
-
-
-// document.querySelector(".gallery").addEventListener("click", function(event) {
-//     event.preventDefault();
-//     const bigImageSrc = event.target.getAttribute('data-source');
-//     if (!Boolean(bigImageSrc)) {
-//            return this;
-//     }
-
-//     console.log(bigImageSrc);
-
-//     const instance = basicLightbox.create(`
-//      <img src="${bigImageSrc}" >
-//  `);
- 
-//  instance.show();
-// });
